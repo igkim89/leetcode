5917c2735abe02854b03afa4fd61aa992e47bcd5
In gyeom <jel4399@gmail.com>
2022-06-20 10:44:11 +0900

    Time: 806 ms (9.96%), Space: 0B (100.00%) - LeetHub

---
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select w1.id
+from Weather w1, Weather w2
+where w1.recordDate = date_sub(w2.recordDate, interval -1 day)
+and w1.temperature > w2.temperature;
