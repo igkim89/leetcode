09247b8e6997e259e4596dbcb46dbf555591441c
In gyeom <jel4399@gmail.com>
2022-07-04 16:33:22 +0900

    Time: 498 ms (61.28%), Space: 0B (100.00%) - LeetHub

---
+++ 597-friend-requests-i-overall-acceptance-rate/597-friend-requests-i-overall-acceptance-rate.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+select round(
+        ifnull(
+            (select count(distinct requester_id, accepter_id) from RequestAccepted)
+            /
+            (select count(distinct sender_id, send_to_id) from FriendRequest)
+            , 0.00
+        )
+    ,2
+    ) as accept_rate
+
+
+
+
+
