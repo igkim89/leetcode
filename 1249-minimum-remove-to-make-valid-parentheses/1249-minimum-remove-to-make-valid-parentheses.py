4146570f71abb693a798bf3993d1be901b06a2fb
In gyeom <jel4399@gmail.com>
2022-07-07 16:38:25 +0900

    Time: 234 ms (26.79%), Space: 16.2 MB (27.76%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,24 +1,41 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        open_stack = []
-        close_stack = []
-        remove_index = []
-        result = ""
+#         open_stack = []
+#         close_stack = []
+#         remove_index = []
+#         result = ""
         
-        for i, c in enumerate(s):
-            if c == "(":
-                open_stack.append(i)
-            elif c == ")":
-                if open_stack:
-                    open_stack.pop()
-                else:
-                    close_stack.append(i)
+#         for i, c in enumerate(s):
+#             if c == "(":
+#                 open_stack.append(i)
+#             elif c == ")":
+#                 if open_stack:
+#                     open_stack.pop()
+#                 else:
+#                     close_stack.append(i)
         
-        remove_list = open_stack + close_stack
+#         remove_list = open_stack + close_stack
              
-        for i, c in enumerate(s):
-            if i not in remove_list:
-                result += c
+#         for i, c in enumerate(s):
+#             if i not in remove_list:
+#                 result += c
             
-        return result
+#         return result
                     
+        indexes_to_remove = set()
+        stack = []
+        for i, c in enumerate(s):
+            if c not in "()":
+                continue
+            if c == "(":
+                stack.append(i)
+            elif not stack:
+                indexes_to_remove.add(i)
+            else:
+                stack.pop()
+        indexes_to_remove = indexes_to_remove.union(set(stack))
+        string_builder = []
+        for i, c in enumerate(s):
+            if i not in indexes_to_remove:
+                string_builder.append(c)
+        return "".join(string_builder)
