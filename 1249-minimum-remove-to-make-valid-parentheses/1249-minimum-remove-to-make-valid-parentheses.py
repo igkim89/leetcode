7bd2f74ba7f9a4fbb715a5c9b939b3c6876d4611
In gyeom <jel4399@gmail.com>
2022-07-07 16:46:15 +0900

    Time: 112 ms (85.98%), Space: 15.4 MB (83.42%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -20,21 +20,3 @@ class Solution:
                 result += c
             
         return result
-                    
-        # indexes_to_remove = set()
-        # stack = []
-        # for i, c in enumerate(s):
-        #     if c not in "()":
-        #         continue
-        #     if c == "(":
-        #         stack.append(i)
-        #     elif not stack:
-        #         indexes_to_remove.add(i)
-        #     else:
-        #         stack.pop()
-        # indexes_to_remove = indexes_to_remove.union(set(stack))
-        # string_builder = []
-        # for i, c in enumerate(s):
-        #     if i not in indexes_to_remove:
-        #         string_builder.append(c)
-        # return "".join(string_builder)
