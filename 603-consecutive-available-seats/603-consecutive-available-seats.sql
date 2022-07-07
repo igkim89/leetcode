4f71abcf0c93484cf7f265c8b289640e0082176c
In gyeom <jel4399@gmail.com>
2022-07-07 17:42:41 +0900

    Time: 304 ms (94.49%), Space: 0B (100.00%) - LeetHub

--- 603-consecutive-available-seats/603-consecutive-available-seats.sql
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
 
 select distinct a.seat_id
-from Cinema a, Cinema b
+from Cinema a join Cinema b
+on abs(a.seat_id - b.seat_id) = 1
 where 
     a.free = 1
     and b.free = 1
-    and (
-        a.seat_id + 1 = b.seat_id
-        or a.seat_id - 1 = b.seat_id
-    )
+
 order by seat_id
