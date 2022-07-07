1a184e9b825f67e2448c6113369a5831d048412e
In gyeom <jel4399@gmail.com>
2022-07-07 16:44:53 +0900

    Time: 123 ms (78.87%), Space: 15.5 MB (75.61%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,41 +1,40 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-#         open_stack = []
-#         close_stack = []
-#         remove_index = []
-#         result = ""
+        open_stack = []
+        remove_set = set()
+        result = ""
         
-#         for i, c in enumerate(s):
-#             if c == "(":
-#                 open_stack.append(i)
-#             elif c == ")":
-#                 if open_stack:
-#                     open_stack.pop()
-#                 else:
-#                     close_stack.append(i)
-        
-#         remove_list = open_stack + close_stack
-             
-#         for i, c in enumerate(s):
-#             if i not in remove_list:
-#                 result += c
-            
-#         return result
-                    
-        indexes_to_remove = set()
-        stack = []
         for i, c in enumerate(s):
-            if c not in "()":
-                continue
             if c == "(":
-                stack.append(i)
-            elif not stack:
-                indexes_to_remove.add(i)
-            else:
-                stack.pop()
-        indexes_to_remove = indexes_to_remove.union(set(stack))
-        string_builder = []
+                open_stack.append(i)
+            elif c == ")":
+                if open_stack:
+                    open_stack.pop()
+                else:
+                    remove_set.add(i)
+        
+        remove_set = remove_set.union(set(open_stack))
+             
         for i, c in enumerate(s):
-            if i not in indexes_to_remove:
-                string_builder.append(c)
-        return "".join(string_builder)
+            if i not in remove_set:
+                result += c
+            
+        return result
+                    
+        # indexes_to_remove = set()
+        # stack = []
+        # for i, c in enumerate(s):
+        #     if c not in "()":
+        #         continue
+        #     if c == "(":
+        #         stack.append(i)
+        #     elif not stack:
+        #         indexes_to_remove.add(i)
+        #     else:
+        #         stack.pop()
+        # indexes_to_remove = indexes_to_remove.union(set(stack))
+        # string_builder = []
+        # for i, c in enumerate(s):
+        #     if i not in indexes_to_remove:
+        #         string_builder.append(c)
+        # return "".join(string_builder)
