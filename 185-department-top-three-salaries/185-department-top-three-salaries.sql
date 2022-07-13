71ce81bf1bec1396cad081426d1a08a599e594dc
In gyeom <jel4399@gmail.com>
2022-07-13 10:34:15 +0900

    Time: 1066 ms (44.58%), Space: 0B (100.00%) - LeetHub

--- 185-department-top-three-salaries/185-department-top-three-salaries.sql
+++ 185-department-top-three-salaries/185-department-top-three-salaries.sql
@@ -1,15 +1,25 @@
 # Write your MySQL query statement below
 
-with three as (
-    select *, dense_rank() over (partition by departmentId order by salary desc) as 'Rank'
-    from Employee
-)
+# with three as (
+#     select *, dense_rank() over (partition by departmentId order by salary desc) as 'Rank'
+#     from Employee
+# )
 
+# select 
+#     b.name as Department, a.name as Employee, a.salary as Salary 
+# from 
+#     three a left join Department b
+#     on a.departmentId = b.id
+# where
+#     a.Rank <= 3
+    
 select 
     b.name as Department, a.name as Employee, a.salary as Salary 
 from 
-    three a left join Department b
+    (
+        select *, dense_rank() over (partition by departmentId order by salary desc) as 'Rank'
+        from Employee
+    ) a left join Department b
     on a.departmentId = b.id
 where
     a.Rank <= 3
-    
