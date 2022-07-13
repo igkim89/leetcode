39d51397be701da48304f50e7ff3a6f776fd8efa
In gyeom <jel4399@gmail.com>
2022-07-13 10:22:10 +0900

    Time: 1029 ms (47.25%), Space: 0B (100.00%) - LeetHub

---
+++ 185-department-top-three-salaries/185-department-top-three-salaries.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+with three as (
+    select *, dense_rank() over (partition by departmentId order by salary desc) as 'Rank'
+    from Employee
+)
+
+select 
+    b.name as Department, a.name as Employee, a.salary as Salary 
+from 
+    three a left join Department b
+    on a.departmentId = b.id
+where
+    a.Rank <= 3
+    
